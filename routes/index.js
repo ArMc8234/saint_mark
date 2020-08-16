ea7cc16bb652939a2b8832249357cfab584701e6
Aaron McKoy <mckoy_aaron@yahoo.com>
2020-08-15 21:45:43 -0400

    Added navbar changes, color scheme, intro image

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Saint Mark's UCC" });
+  res.render('index', { title: "Saint Mark United Church of Christ" });
 });
 
 module.exports = router;
