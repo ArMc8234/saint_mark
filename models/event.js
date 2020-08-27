fd933978cc95c6e370a61ae9c3856cc270f92a50
Aaron McKoy <mckoy_aaron@yahoo.com>
2020-08-27 00:51:12 -0400

    Required momentjs in event model.

--- models/event.js
+++ models/event.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
 const Schema = mongoose.Schema;
-// const moment = require("moment");
+
 
 
 const eventSchema = new Schema({
