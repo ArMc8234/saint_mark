e8892f185a8b3d04ebb41ef413f23bfa41cb10bc
Aaron McKoy <mckoy_aaron@yahoo.com>
2020-09-21 00:37:24 -0400

    Event creation fixed

--- public/javascripts/mainpage.js
+++ public/javascripts/mainpage.js
@@ -2,6 +2,32 @@
 // const eventsController = require("../../controllers/eventsController");
 
 $(document).ready(function(){
+  $('#submit').on('click', function (event) {
+    event.preventDefault();
+
+    
+    var newEvent = {
+      title: $('#title').val().trim(),
+      date: $('#date').val().trim(),
+      start: $('#start').val().trim(),
+      end: $('#end').val().trim(),
+      description: $('#description').val().trim(),
+    }
+    console.log(newEvent);
+    $.ajax({
+      method: "POST",
+      url: "/api/events",
+      data: newEvent,
+    }).then(function (data) {
+      console.log(data);
+      // location.reload(true);
+    });
+    console.log("Form Data Sent");
+
+  }); 
+
+
+
     //set  document to listen for <td> hyperlink click and call for the event description linked to the same ID as the event title"
    $(document).on('click', "#eventLink",  function(){
       let currentTitle = $(this).html();
