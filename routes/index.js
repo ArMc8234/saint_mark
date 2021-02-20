b622bccf13c4dab4f8c0e5429cf59b90541c312e
Aaron McKoy <mckoy_aaron@yahoo.com>
2021-02-20 15:17:40 -0500

    Created ministries and mission views

--- routes/index.js
+++ routes/index.js
@@ -118,6 +118,14 @@ router.get('/', function(req, res, next) {
 router.get('/pastor', function(req, res, next) {
   return res.render('pastor', { title: "Meet St. Mark's Pastor" });
 });
+// GET /staff
+router.get('/ministries', function(req, res, next) {
+  return res.render('ministries', { title: "Ministries" });
+});
+// GET /staff
+router.get('/mission', function(req, res, next) {
+  return res.render('mission', { title: "Mission" });
+});
 //GET the Event form page
 // router.get('/events', function(req, res, next) {
 //   db.Event.find({})
